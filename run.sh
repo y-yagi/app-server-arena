7e03bd9223903e34d732ac5ebb1ad8f94632f055
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-01-12 15:06:49 +0900

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-SERVER=rhebok
-#siege -r 1000 -c 100 -b -q  http://localhost:3000/pi > ${SERVER}_pi.txt 2>&1
-siege -r 10 -c 10 -b -q  http://localhost:3000/sleep  > performance_ruby_2_2_0/${SERVER}/sleep.txt 2>&1
-siege -r 10 -c 10 -b -q  http://localhost:3000/pi     > performance_ruby_2_2_0/${SERVER}/pi.txt 2>&1
-siege -r 10 -c 10 -b -q  http://localhost:3000/server > performance_ruby_2_2_0/${SERVER}/server.txt 2>&1
-siege -r 10 -c 10 -b -q  http://localhost:3000/random > performance_ruby_2_2_0/${SERVER}/random.txt 2>&1
+# passenger / puma / raptor(passenger 5) / rhebok / thin /unicorn
+SERVER=unicorn
+REQUEST=10
+CLIENT=10
+
+siege -r ${REQUEST} -c ${CLIENT} -b -q  http://localhost:3000/sleep  > performance_ruby_2_2_0/${SERVER}/sleep.txt 2>&1
+siege -r ${REQUEST} -c ${CLIENT} -b -q  http://localhost:3000/pi     > performance_ruby_2_2_0/${SERVER}/pi.txt 2>&1
+siege -r ${REQUEST} -c ${CLIENT} -b -q  http://localhost:3000/server > performance_ruby_2_2_0/${SERVER}/server.txt 2>&1
+siege -r ${REQUEST} -c ${CLIENT} -b -q  http://localhost:3000/random > performance_ruby_2_2_0/${SERVER}/random.txt 2>&1
