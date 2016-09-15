7fe01422d1477c294a590bd030ca55d6191412f9
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2016-09-15 21:11:51 +0300

    update tests

--- run.sh
+++ run.sh
@@ -1,12 +1,49 @@
 #!/bin/sh
 
-# passenger / puma / raptor(passenger 5) / rhebok / thin /unicorn
-SERVER=rhebok
-REQUEST=1000
-CLIENT=100
+SERVERS=(passenger puma Rhebok thin unicorn)
 
-siege -r ${REQUEST} -c ${CLIENT} -b -q  http://localhost:3000/sleep  > performance_ruby_2_2_0/${SERVER}/sleep.txt 2>&1
-siege -r ${REQUEST} -c ${CLIENT} -b -q  http://localhost:3000/pi     > performance_ruby_2_2_0/${SERVER}/pi.txt 2>&1
-siege -r ${REQUEST} -c ${CLIENT} -b -q  http://localhost:3000/server > performance_ruby_2_2_0/${SERVER}/server.txt 2>&1
-siege -r ${REQUEST} -c ${CLIENT} -b -q  http://localhost:3000/random > performance_ruby_2_2_0/${SERVER}/random.txt 2>&1
-siege -r ${REQUEST} -c ${CLIENT} -b -q  http://localhost:3000/active_record > performance_ruby_2_2_0/${SERVER}/active_record.txt 2>&1
+# stop servers
+pkill -f ruby &> /dev/null
+
+# seeds
+ruby ./setup.rb &> /dev/null
+
+# run servers
+for i in "${!SERVERS[@]}"; do
+  rackup -s ${SERVERS[$i]} --port "300$i" -E production config.ru &> /dev/null &
+done
+sleep 10
+
+# warm
+for i in "${!SERVERS[@]}"; do
+  curl -s -o /dev/null -I -w "%{http_code}\n" "http://localhost:300$i/"
+done
+
+# test
+echo active_record
+for i in "${!SERVERS[@]}"; do
+  wrk -c 10 -d 10s -t 10 "http://localhost:300$i/active_record" > "performance/${SERVERS[$i]}/active_record.txt" 2>&1
+done
+
+echo pi
+for i in "${!SERVERS[@]}"; do
+  wrk -c 10 -d 10s -t 10 "http://localhost:300$i/pi" > "performance/${SERVERS[$i]}/pi.txt" 2>&1
+done
+
+echo random
+for i in "${!SERVERS[@]}"; do
+  wrk -c 10 -d 10s -t 10 "http://localhost:300$i/random" > "performance/${SERVERS[$i]}/random.txt" 2>&1
+done
+
+echo server
+for i in "${!SERVERS[@]}"; do
+  wrk -c 10 -d 10s -t 10 "http://localhost:300$i/server" > "performance/${SERVERS[$i]}/server.txt" 2>&1
+done
+
+echo sleep
+for i in "${!SERVERS[@]}"; do
+  wrk -c 10 -d 10s -t 10 "http://localhost:300$i/sleep" > "performance/${SERVERS[$i]}/sleep.txt" 2>&1
+done
+
+# stop servers
+pkill -f ruby &> /dev/null
--- setup.rb
+++ setup.rb
@@ -5,15 +5,17 @@ require 'active_record'
 require 'logger'
 require_relative 'user'
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'tmp/db')
 ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'tmp/db')
 
 ActiveRecord::Schema.define do
+  drop_table :users
   create_table :users do |t|
     t.string :name
     t.string :email
   end
 
+  drop_table :comments
   create_table :comments do |t|
     t.integer :post_id
   end
@@ -22,4 +24,3 @@ end
 1000.times do |i|
   User.create(name: "name_#{i}", email: "name_#{i}@example.com")
 end
-
